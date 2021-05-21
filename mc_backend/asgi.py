a1ffdbc37232b0c4a7d73b8d208d80dfe9065ff6
Ubuntu <ubuntu@ip-172-31-32-173.ap-south-1.compute.internal>
2021-05-21 14:40:18 +0000

    asgi and settings are ready for production

--- mc_backend/asgi.py
+++ mc_backend/asgi.py
@@ -1,13 +1,18 @@
 import os
+from django.conf.urls import url
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mc_backend.settings')
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack   
 import api_backend.routing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mc_backend.settings')
+
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             api_backend.routing.websocket_urlpatterns
--- mc_backend/settings.py
+++ mc_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ndstv7raxf7^w5*kw-niwai^l(zc#_2p-+wt5pvrc!o*bhy4qd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','65.2.126.93','localhost']
 
 
 # Application definition
@@ -119,22 +119,22 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'chat_db',
-    'USER': 'postgres',
-    'PASSWORD':'1717resh',
-    'HOST': 'localhost',
-    'PORT': '5432',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+#DATABASES = {
+#'default': {
+#    'ENGINE': 'django.db.backends.postgresql',
+#    'NAME': 'chat_db',
+#    'USER': 'postgres',
+#    'PASSWORD':'1717resh',
+#    'HOST': 'localhost',
+#    'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
@@ -174,3 +174,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
